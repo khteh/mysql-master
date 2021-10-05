4893ae59f01a959a93005710da56ba06b392e29b
Kok How, Teh <funcoolgeek@gmail.com>
2021-10-05 17:35:50 +0800

    Use /etc/mysql/conf.d/mysql.cnf

--- server-id.sh
+++ server-id.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export INDEX=${HOSTNAME##*-}
 export ID=$(( $INDEX + 1 ))
-sed -i '/\[mysqld\]/a server_id='$ID'' /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i '/\[mysqld\]/a server_id='$ID'' /etc/mysql/conf.d/mysql.cnf
